16eb44a77567f4dfbc3cee3be05447de7461f5b2
wisoot <wisoot@gmail.com>
2016-02-05 10:27:22 +1100

    Add embed manager and move resource test cases into their own directory

---
+++ src/EmbedManager.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace WWON\Restify;
+
+use WWON\Restify\Resource\Embed;
+
+class EmbedManager
+{
+
+    /**
+     * filter method
+     *
+     * @param array $embeds
+     * @param array $filteringEmbeds
+     * @return array
+     */
+    public function filter(array $embeds, array $filteringEmbeds)
+    {
+        $results = [];
+
+        foreach ($filteringEmbeds as $filteringEmbed) {
+            foreach ($embeds as $embed) {
+                if ($filteringEmbed->name == $embed->name) {
+                    $results[] = $this->deepFilter($embed, $filteringEmbed);
+                }
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * deepFilter method
+     *
+     * @param Embed $embed
+     * @param Embed $filteringEmbed
+     * @return Embed
+     */
+    protected function deepFilter(Embed $embed, Embed $filteringEmbed)
+    {
+        $children = [];
+
+        foreach ($filteringEmbed->children as $filteringEmbedChild) {
+            foreach ($embed->children as $embedChild) {
+                if ($filteringEmbedChild->name == $embedChild->name) {
+                    $children[] = $this->deepFilter($embedChild, $filteringEmbedChild);
+                }
+            }
+        }
+
+        $filteringEmbed->children = $children;
+
+        return $filteringEmbed;
+    }
+
+    /**
+     * getEmbedsFromParam method
+     *
+     * @param string $embedString
+     * @return array
+     */
+    public function getEmbedsFromParam($embedString)
+    {
+        $embeds = [];
+
+        $items = explode(',', $embedString);
+
+        foreach ($items as $item) {
+            $embeds[] = $this->getAnEmbed(explode('.', $item));
+        }
+
+        return $embeds;
+    }
+
+    /**
+     * getAnEmbed method
+     *
+     * @param array $embedItems
+     * @return Embed
+     */
+    protected function getAnEmbed(array $embedItems = [])
+    {
+        $item = array_shift($embedItems);
+
+        if (count($embedItems)) {
+            $children = [$this->getAnEmbed($embedItems)];
+        } else {
+            $children = [];
+        }
+
+        return new Embed($item, $children);
+    }
+
+}
---
+++ tests/EmbedManagerTest.php
@@ -0,0 +1,145 @@
+<?php
+
+use WWON\Restify\EmbedManager;
+use WWON\Restify\Resource\Embed;
+
+class EmbedManagerTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var EmbedManager
+     */
+    private $embedManager;
+
+    /**
+     * setUp method
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->embedManager = new EmbedManager();
+    }
+
+    /**
+     * tearDown method
+     */
+    public function tearDown()
+    {
+        unset($this->embedManager);
+
+        parent::tearDown();
+    }
+
+    public function testFilter()
+    {
+        // All empty
+        $embeds = [];
+        $filteringEmbeds = [];
+        $results = $this->embedManager->filter($embeds, $filteringEmbeds);
+
+        $this->assertEquals([], $results);
+
+        // Master list is empty
+        $embeds = [];
+        $filteringEmbeds = [new Embed('comments')];
+        $results = $this->embedManager->filter($embeds, $filteringEmbeds);
+
+        $this->assertEquals([], $results);
+
+        // Same list with 1 element
+        $embeds = [new Embed('comments')];
+        $filteringEmbeds = [new Embed('comments')];
+        $results = $this->embedManager->filter($embeds, $filteringEmbeds);
+
+        $this->assertEquals(1, count($results));
+        $this->assertEquals('comments', $results[0]->name);
+        $this->assertEquals([], $results[0]->children);
+
+        // Master list has more stuff
+        $embeds = [new Embed('comments', [new Embed('users')]), new Embed('likes')];
+        $filteringEmbeds = [new Embed('comments')];
+        $results = $this->embedManager->filter($embeds, $filteringEmbeds);
+
+        $this->assertEquals(1, count($results));
+        $this->assertEquals('comments', $results[0]->name);
+        $this->assertEquals([], $results[0]->children);
+
+        // Input list has more stuff
+        $embeds = [new Embed('comments')];
+        $filteringEmbeds = [new Embed('comments', [new Embed('users')])];
+        $results = $this->embedManager->filter($embeds, $filteringEmbeds);
+
+        $this->assertEquals(1, count($results));
+        $this->assertEquals('comments', $results[0]->name);
+        $this->assertEquals([], $results[0]->children);
+
+        // Input list has different stuff compare to master
+        $embeds = [new Embed('likes')];
+        $filteringEmbeds = [new Embed('comments')];
+        $results = $this->embedManager->filter($embeds, $filteringEmbeds);
+
+        $this->assertEquals([], $results);
+
+        // Input list has different stuff compare to master on children level
+        $embeds = [new Embed('comments', [new Embed('likes')])];
+        $filteringEmbeds = [new Embed('comments', [new Embed('users')])];
+        $results = $this->embedManager->filter($embeds, $filteringEmbeds);
+
+        $this->assertEquals(1, count($results));
+        $this->assertEquals('comments', $results[0]->name);
+        $this->assertEquals([], $results[0]->children);
+
+        // Input list has different stuff compare to master on children level pretty complex one
+        $embeds = [new Embed('comments', [new Embed('users')]), new Embed('likes', [new Embed('users')])];
+        $filteringEmbeds = [new Embed('comments', [new Embed('users')]), new Embed('likes')];
+        $results = $this->embedManager->filter($embeds, $filteringEmbeds);
+
+        $this->assertEquals(2, count($results));
+        $this->assertEquals('comments', $results[0]->name);
+        $this->assertEquals(1, count($results[0]->children));
+        $this->assertEquals('users', $results[0]->children[0]->name);
+        $this->assertEquals('likes', $results[1]->name);
+        $this->assertEquals([], $results[1]->children);
+
+        // Input list has different stuff compare to master on children, oh what a mess!
+        $embeds = [new Embed('comments', [new Embed('users')]), new Embed('likes', [new Embed('users')])];
+        $filteringEmbeds = [new Embed('comments', [new Embed('likes')]), new Embed('likes', [new Embed('guns')])];
+        $results = $this->embedManager->filter($embeds, $filteringEmbeds);
+
+        $this->assertEquals(2, count($results));
+        $this->assertEquals('comments', $results[0]->name);
+        $this->assertEquals([], $results[0]->children);
+        $this->assertEquals('likes', $results[1]->name);
+        $this->assertEquals([], $results[1]->children);
+    }
+
+    public function testGetEmbedsFromParam()
+    {
+        $embeds = $this->embedManager->getEmbedsFromParam('comments');
+
+        $this->assertEquals(1, count($embeds));
+        $this->assertEquals('comments', $embeds[0]->name);
+        $this->assertEquals([], $embeds[0]->children);
+
+        $embeds = $this->embedManager->getEmbedsFromParam('games.teams.players');
+
+        $this->assertEquals(1, count($embeds));
+        $this->assertEquals('games', $embeds[0]->name);
+        $this->assertEquals(1, count($embeds[0]->children));
+        $this->assertEquals('teams', $embeds[0]->children[0]->name);
+        $this->assertEquals(1, count($embeds[0]->children[0]->children));
+        $this->assertEquals('players', $embeds[0]->children[0]->children[0]->name);
+
+        $embeds = $this->embedManager->getEmbedsFromParam('comments.users,likes.users');
+
+        $this->assertEquals(2, count($embeds));
+        $this->assertEquals('comments', $embeds[0]->name);
+        $this->assertEquals(1, count($embeds[0]->children));
+        $this->assertEquals('users', $embeds[0]->children[0]->name);
+        $this->assertEquals('likes', $embeds[1]->name);
+        $this->assertEquals(1, count($embeds[1]->children));
+        $this->assertEquals('users', $embeds[1]->children[0]->name);
+    }
+
+}
